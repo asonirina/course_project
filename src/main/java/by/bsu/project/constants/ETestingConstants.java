bf5a489b658754bb6629980355d83728bf4aa6c4
LinaKorn <alina.glumova@gmail.com>
2013-02-22 20:53:59 +0300

    Adding classes for constants

---
+++ src/main/java/by/bsu/project/constants/ETestingConstants.java
@@ -0,0 +1,20 @@
+package by.bsu.project.constants;
+
+public class ETestingConstants {
+    public final static String POSTFIX_C          = ".c";
+    public final static String POSTFIX_CPP        = ".cpp";
+    public final static String POSTFIX_PASCAL_P   = ".p";
+    public final static String POSTFIX_PASCAL_PAS = ".pas";
+
+    public final static String ADMIN_ROLE         = "admin";
+
+    public final static String MODEL_ERRORS       = "errors";
+    public final static String MODEL_STUDENT      = "student";
+    public final static String MODEL_STUDENT_LIST = "studentList";
+    public final static String MODEL_MESSAGE      = "message";
+    public final static String MODEL_PROGRAM_LIST = "programList";
+    public final static String MODEL_PROGRAM      = "program";
+
+    public final static String TABLE_FIELD_LOGIN  = "login";
+    public final static String TABLE_FIELD_ID     = "id";
+}
---
+++ src/main/java/by/bsu/project/constants/ErrorsMessages.java
@@ -0,0 +1,11 @@
+package by.bsu.project.constants;
+
+public class ErrorsMessages {
+    public final static String UNLOAD_FILE           = "Загрузите файл на проверку!";
+    public final static String VALID_OPTIONS         = "Допустимые расширения файлов *.c, *.cpp, *.p или *.pas";
+    public final static String PROGRAM_NAME_REQUIRED = "Укажите название лабораторной.";
+    public final static String UNIQUE_LOGIN          = "Логин должен быть уникальным. Такой логин есть в базе.";
+    public final static String WRONG_PASSWORD        = "Пароль введен не верно!";
+
+    public final static String INVALID_USERNAME      = "Invalid username or password or username is blocked.";
+}
