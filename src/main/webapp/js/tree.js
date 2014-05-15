7f575ef1a2bcf4d7a7caaccf6579b3f58d03527e
iason <asonirina@mail.ru>
2014-05-15 17:01:34 +0300

    styles

---
+++ src/main/webapp/js/tree.js
@@ -0,0 +1,163 @@
+var dataMap = data.reduce(function (map, node) {
+    map[node.name] = node;
+    return map;
+}, {});
+
+var treeData = [];
+data.forEach(function (node) {
+    var parent = dataMap[node.parent];
+    if (parent) {
+        (parent.children || (parent.children = []))
+            .push(node);
+    } else {
+        treeData.push(node);
+    }
+});
+
+var ost = (window.innerWidth - 850) / 2;
+var margin = {top:20, right:ost, bottom:20, left:ost},
+    width = window.innerWidth - margin.right - margin.left,
+    height = window.innerHeight - margin.top - margin.bottom;
+
+var i = 0, duration = 750;
+
+var tree = d3.layout.tree()
+    .size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function (d) {
+        return [d.y, d.x];
+    });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+root = treeData[0];
+
+root.x0 = 0;
+root.y0 = height / 2;
+
+function collapse(d) {
+    if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = null;
+    }
+}
+//          root.children.forEach(collapse);
+update(root);
+
+d3.select(self.frameElement).style("height", "800px");
+
+function update(source) {
+
+    var nodes = tree.nodes(root).reverse(),
+        links = tree.links(nodes);
+
+    nodes.forEach(function (d) {
+        d.y = d.depth * 180;
+    });
+
+    var node = svg.selectAll("g.node")
+        .data(nodes, function (d) {
+            return d.id || (d.id = ++i);
+        });
+
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + source.y0 + "," + source.x0 + ")";
+        })
+        .on("click", click);
+
+    nodeEnter.append("circle")
+        .attr("r", 1e-6)
+        .style("fill", function (d) {
+            return d._children ? "red" : "#fff";
+        });
+
+    nodeEnter.append("text")
+        .attr("x", function (d) {
+            return d.children || d._children ? -10 : 10;
+        })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function (d) {
+            return d.children || d._children ? "end" : "start";
+        })
+        .text(function (d) {
+            return d.name;
+        })
+        .style("fill-opacity", 1e-6);
+
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr("transform", function (d) {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+
+    nodeUpdate.select("circle")
+        .attr("r", 4.5)
+        .style("fill", function (d) {
+            return d._children ? "red" : "#fff";
+        });
+
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1);
+
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function (d) {
+            return "translate(" + source.y + "," + source.x + ")";
+        })
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 1e-6);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6);
+
+    var link = svg.selectAll("path.link")
+        .data(links, function (d) {
+            return d.target.id;
+        });
+
+    link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function (d) {
+            var o = {x:source.x0, y:source.y0};
+            return diagonal({source:o, target:o});
+        });
+
+    link.transition()
+        .duration(duration)
+        .attr("d", diagonal);
+
+    link.exit().transition()
+        .duration(duration)
+        .attr("d", function (d) {
+            var o = {x:source.x, y:source.y};
+            return diagonal({source:o, target:o});
+        })
+        .remove();
+
+    nodes.forEach(function (d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+    });
+}
+
+function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children;
+        d._children = null;
+    }
+    update(d);
+}
