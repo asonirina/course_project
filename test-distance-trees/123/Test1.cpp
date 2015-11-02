e5f9cf04209798e1d2a48bacd83c185cf24d6b70
iason <asonirina@mail.ru>
2015-11-02 16:42:20 +0300

    add test files

---
+++ test-distance-trees/123/Test1.cpp
@@ -0,0 +1,10 @@
+#include <fstream>
+using namespace std;
+
+int main()
+{
+   ofstream fout("out.txt");
+   fout << "1 2 3";
+   fout <<endl;
+
+}
---
+++ test-distance-trees/123/Test1234.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class Test1234 {
+
+public static int yyy(int zz) {
+
+return 0;
+}
+	public static void main(String[] args) {
+
+Writer w = new Writer();
+w.write("123");
+
+
+
+int u = yyy(1) + yyy(2);
+		
+	}
+}
